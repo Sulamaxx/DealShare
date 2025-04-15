0f8b43cad67901d7e098c0b7add6e7b2550922c7
SulaMaxx <sjeewaranga@gmail.com>
2025-04-15 13:15:30 +0530

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+});
+// Route::get('/login', function () {
+//     return view('index');
+// });
+// Route::get('/regiter', function () {
+//     return view('index');
+// });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
