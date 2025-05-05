576c37d0f24ebed6261f283fc692374bbd21c164
SulaMaxx <sjeewaranga@gmail.com>
2025-05-06 01:25:24 +0530

    new migrations

---
+++ database/migrations/2025_05_05_195410_create_notifications_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('type', ['new_reply', 'reply_to_comment', 'thread_update']);
+            $table->json('data'); // contains post_id, comment_id, message etc.
+            $table->boolean('is_read')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
